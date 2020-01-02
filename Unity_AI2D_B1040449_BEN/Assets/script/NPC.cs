1c1e9240448d07f598934a6b83a0e7cd613435e0
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2020-01-02 14:38:49 +0800

    1002

---
+++ Unity_AI2D_B1040449_BEN/Assets/script/NPC.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    public enum state
+    {
+        start, notComplete, complete
+    }
+
+    public state _state;
+
+    public string sayStart = "能找10隻魚給我嗎";
+    public string sayNotComplete = "還沒找到5之魚，等全找到再來找我吧";
+    public string sayComplete = "謝了老兄";
+
+    public float speed = 0.1f;
+
+    public bool complete;
+    public int countPlayer;
+    public int countFinish = 10;
+
+    public GameObject objCanvas;
+    public Text textSay;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "忍者")
+            Say();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "忍者")
+            SayClose();
+    }
+
+    private void Say()
+    {
+        objCanvas.SetActive(true);
+        StopAllCoroutines();
+
+        if (countPlayer >= countFinish) _state = state.complete;
+
+
+        switch (_state)
+        {
+            case state.start:
+                StartCoroutine(ShowDialog(sayStart));           
+                _state = state.notComplete;
+                break;
+            case state.notComplete:
+                StartCoroutine(ShowDialog(sayNotComplete));     
+                break;
+            case state.complete:
+                StartCoroutine(ShowDialog(sayComplete));        
+                break;
+        }
+    }
+
+    private IEnumerator ShowDialog(string say)
+    {
+        textSay.text = "";                              
+
+        for (int i = 0; i < say.Length; i++)            
+        {
+            textSay.text += say[i].ToString();          
+            yield return new WaitForSeconds(speed);     
+        }
+    }
+
+    private void SayClose()
+    {
+        StopAllCoroutines();
+        objCanvas.SetActive(false);
+    }
+
+    public void PlayerGet()
+    {
+        countPlayer++;
+    }
+}
---
+++ Unity_AI2D_B1040449_BEN/Assets/script/PLAY.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PLAY : MonoBehaviour
+{
+    public int speed = 50;                  
+    public float jump = 50f;                        
+    public bool pass = false;               
+    public bool isGround = false;
+    public float hp = 100;
+    public UnityEvent onEat;
+    private Rigidbody2D r2d;
+    private Transform tra;
+    public Image hpBar;
+    private float hpmax;
+    public GameObject final;
+
+    private void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        tra = GetComponent<Transform>();
+
+        hpmax = hp;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) Turn(0);
+        if (Input.GetKeyDown(KeyCode.A)) Turn(180);
+    }
+
+    private void FixedUpdate()
+    {
+        Walk();   
+        Jump();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("碰到" + collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "魚")
+        {
+            Destroy(collision.gameObject);
+            onEat.Invoke();
+        }
+    }
+
+
+    void Walk()
+    {
+        r2d.AddForce(new Vector2(speed * (Input.GetAxis("Horizontal")), 0));
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            r2d.AddForce(new Vector2(0, jump));
+        }
+    }
+
+    void Turn(int direction)
+    {
+        tra.eulerAngles = new Vector3(0, direction, 0);
+    }
+
+    public void Damage(float damage)
+    {
+        hp -= damage;
+        hpBar.fillAmount = hp / hpmax;
+
+        //死亡
+        if (hp <= 0) final.SetActive(true);
+    }
+}
